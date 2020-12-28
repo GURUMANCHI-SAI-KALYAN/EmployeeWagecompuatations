8ecca68cafbb1aaa079775009c5b0fe6a0d0d585
GURUMANCHI-SAI-KALYAN <kalyangurumanchi@gmail.com>
2020-12-28 18:39:08 +0530

    ADD.added empfunctionuc7 file

--- empWagecompuatations.sh
+++ empWagecompuatations.sh
@@ -1,30 +1,37 @@
 #!/bin/bash -x
+#! /bin/bash -x
 parttime=1
 fulltime=2
 rateperhr=20
 maxdays=20
 maxhr=50
 days=0
-while [[ $days -lt $maxdays && $emphr -lt $maxhr ]]
-do
-((days++))
-empcheck=$((RANDOM%3))
-
-case $empcheck in
+emphr=0
+function employhrs() {
+        local empcheck=$1
+        case $empcheck in
         $parttime)
-                ((emphr+=4))
+                emphr=4
                 ;;
         $fulltime)
-                 ((emphr+=8))
+                 emphr=8
                 ;;
 
                 *)
-                  ((emphr+=0))
+                  emphr=0
                 ;;
-esac
-        salary=$(($rateperhr*$emphr))
-        totsalary=$(($totsalary+$salary))
+        esac
+        echo $emphr
+}
+
+while [[ $days -lt $maxdays && $emphr -lt $maxhr ]]
+do
+        ((days++))
+        emphr="$( employhrs $((RANDOM%3)) )"
+        salary=$((rateperhr*emphr))
+        totsalary=$((totsalary+salary))
 done
+
 
 
 
